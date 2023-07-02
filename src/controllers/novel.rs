db47b9364815b816fe298f26c3340ffeee29a04e
Raiyol <dn-a@live.fr>
2023-07-02 23:43:59 +0200

    Adding object validation

--- src/controllers/novel.rs
+++ src/controllers/novel.rs
@@ -1,7 +1,7 @@
 use actix_web::{error, HttpResponse, Responder, web};
 use diesel::prelude::*;
 use diesel::query_dsl::QueryDsl;
-use validator::Validate;
+use actix_web_validator::Json;
 
 use crate::beans::pageable::Pageable;
 use crate::db::DbPool;
@@ -10,11 +10,11 @@ use crate::models::novel::Novel;
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::resource("/")
-            .route(web::get().to(get_novels))
+            .route(web::post().to(get_novels))
     );
 }
 
-async fn get_novels(pool: web::Data<DbPool>, pageable: web::Json<Pageable>) -> actix_web::Result<impl Responder> {
+async fn get_novels(pool: web::Data<DbPool>, pageable: Json<Pageable>) -> actix_web::Result<impl Responder> {
     use crate::schema::schema::novel::dsl::*;
     let novels = web::block(move || {
         let mut conn = pool.get().expect("Error getting connection to DB");
